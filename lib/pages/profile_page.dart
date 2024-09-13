629c2f4e759140ba29986f8c842fd1e5ced1e127
Mei Rusfandi <mei.rusfandi@dto.kemkes.go.id>
2024-09-13 21:29:04 +0700

    create based project

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  final String routeName = '/profile';
+  const ProfilePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class Utils {
+  // color
+  static Color primaryColor = const Color(0xFFF8C20A);
+  static Color selectedColor = const Color(0xFFDAB958);
+  static Color blackColor = const Color(0xFF212528);
+  static Color tealColor = const Color(0xFF2D7794);
+
+}
